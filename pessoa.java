a1295f797bc521104fed997e3a8ff3d19674de6a
Pedrö <106125456+Pedro260103@users.noreply.github.com>
2023-04-05 09:17:19 -0300

    Create pessoa.java

---
+++ pessoa.java
@@ -0,0 +1,12 @@
+/**
+ * pessoa
+ */
+public class pessoa extends animal{
+  private int salario;
+
+  public pessoa(String nome , int idade ,String CPF){
+    super(nome,idade,CPF);  
+    this.salario = salario;
+  }
+  
+}
